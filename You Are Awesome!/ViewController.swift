201e6f34999a00601a6871dfa518b26029dc49dd
Connor Sullivan <sulliamr@bc.edu>
2020-01-27 14:26:11 -0500

    2.14 Adding sound

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
@@ -15,6 +16,7 @@ class ViewController: UIViewController {
     var imageNumber = -1
     var messageNumber = -1
     let totalNumberOfImages = 9
+    var audioPlayer = AVAudioPlayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,16 +37,27 @@ class ViewController: UIViewController {
         messageNumber = newMessageNumber
         messageLabel.text = messages[messageNumber]
         
-    
+        
         var newImageNumber: Int
         repeat {
             newImageNumber = Int.random(in: 0...totalNumberOfImages)
         } while imageNumber == newImageNumber
         imageNumber = newImageNumber
         imageView.image = UIImage(named: "image\(imageNumber)")
-        }
         
-
+        if let sound = NSDataAsset(name: "sound0"){
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("Error: \(error.localizedDescription) Couldn't initialize AVAudioPlayer")
+            }
+        } else {
+            print("Error: Could Not Play Sound")
+        }
     }
+    
+    
+}
 
 
